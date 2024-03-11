97f9a2306925d5e109a360320aa7238583b9a927
Borys Melnyk <bor920@gmail.com>
2024-03-11 16:18:20 +0000

    fix: default page width

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,30 +1,40 @@
-'use client'
-import { pdfjs, Document, Page, Outline } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
-import styles from './cv.module.scss';
-import { useWindowSize } from '@/utils/hooks';
+"use client";
+import { pdfjs, Document, Page, Outline } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import styles from "./cv.module.scss";
+import { useWindowSize } from "@/utils/hooks";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.js',
-    import.meta.url,
-  ).toString();
-  
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
 
 const CV = () => {
-    const {width, height} = useWindowSize();
-    const pageWidth = Math.min(width - 160, 1280-160);
-    console.log({pageWidth, width})
-    return (
-        <main className={styles.documentContainer}>
-            
-            <Document className={styles.documentContainer} file="/cv.pdf" onLoadError={(err) => console.error(err)}>
-                <Outline /> 
-                    <Page className={styles.documentContainer} width={pageWidth} pageNumber={1} />
-                    <Page className={styles.documentContainer} width={pageWidth} pageNumber={2} />
-            </Document>
-        </main>
-    )
-}
+  const { width, height } = useWindowSize();
+  const pageWidth = Math.min((width || 1920) - 160, 1280 - 160);
+  console.log({ pageWidth, width });
+  return (
+    <main className={styles.documentContainer}>
+      <Document
+        className={styles.documentContainer}
+        file="/cv.pdf"
+        onLoadError={(err) => console.error(err)}
+      >
+        <Outline />
+        <Page
+          className={styles.documentContainer}
+          width={pageWidth}
+          pageNumber={1}
+        />
+        <Page
+          className={styles.documentContainer}
+          width={pageWidth}
+          pageNumber={2}
+        />
+      </Document>
+    </main>
+  );
+};
 
 export default CV;
