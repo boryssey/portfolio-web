57d1c66c162b8863499cc9ce90e899d320bfa2bb
Borys Melnyk <bor920@gmail.com>
2024-03-14 19:09:53 +0000

    feat: change pdf handling to use iframe

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { pdfjs, Document, Page, Outline } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
@@ -11,29 +10,9 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 ).toString();
 
 const CV = () => {
-  const { width, height } = useWindowSize();
-  console.log("🚀 ~ CV ~ width:", width);
-  const pageWidth = Math.min(width - (width < 768 ? 10 : 160), 1280 - 160);
-  console.log({ pageWidth, width });
   return (
     <main className={styles.documentContainer}>
-      <Document
-        className={styles.documentContainer}
-        file="/cv.pdf"
-        onLoadError={(err) => console.error(err)}
-      >
-        <Outline />
-        <Page
-          className={styles.documentContainer}
-          width={pageWidth}
-          pageNumber={1}
-        />
-        <Page
-          className={styles.documentContainer}
-          width={pageWidth}
-          pageNumber={2}
-        />
-      </Document>
+      <iframe src="/cv.pdf" height="100%" />
     </main>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import Link from "next/link";
-import ArrowTopRight from "@/components/icons/ArrowTopRight";
-
-
 
 export default function Home() {
   return (
@@ -12,27 +9,33 @@ export default function Home() {
       <h3 className={styles.jobTitle}>Software Engineer</h3>
       <div className={styles.description}>
         <p>
-        I am a Full-Stack engineer with 4 years of professional development experience. Specialise in development with React, Next.js, and Express.JS. <br/>
-        Experienced in planning, developing and testing WebApps, Browser Extensions, and APIs.
+          I am a Full-Stack engineer with 4 years of professional development
+          experience. Specialise in development with React, Next.js, and
+          Express.JS. <br />
+          Experienced in planning, developing and testing WebApps, Browser
+          Extensions, and APIs.
         </p>
-        <br/>
+        <br />
         <p>My current tech stack is: </p>
-          <ul>
-            <li>React</li>
-            <li>Next.JS</li>
-            <li>Express.JS</li>
-            <li>PostgreSQL / MongoDB / MySQL</li>
-          </ul>
-        <br/>
-        You can contact me at: <a href={'mailto:jobs@boryssey.com'}>jobs@boryssey.com</a>
-        <br/>
-        <br/>
-        <p>You can read something I wrote in <Link href='/blog'> my blog</Link></p>
+        <ul>
+          <li>React</li>
+          <li>Next.JS</li>
+          <li>Express.JS</li>
+          <li>PostgreSQL / MongoDB / MySQL</li>
+        </ul>
+        <br />
+        You can contact me at:{" "}
+        <a href={"mailto:jobs@boryssey.com"}>jobs@boryssey.com</a>
+        <br />
         <br />
         <p>
-          Or if you are interested in photography, you can check out my <Link href={'/gallery'}>gallery</Link>
+          You can read something I wrote in <Link href="/blog"> my blog</Link>
         </p>
-        
+        <br />
+        <p>
+          Or if you are interested in photography, you can check out my{" "}
+          <Link href={"/gallery"}>gallery</Link>
+        </p>
       </div>
     </main>
   );
