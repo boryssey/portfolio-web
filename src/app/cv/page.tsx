ef6827589ae872f0d955d0118a9465afe372f721
Borys Melnyk <bor920@gmail.com>
2024-03-14 19:36:29 +0000

    feat: use s3 link for cv

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -3,7 +3,10 @@ import styles from "./cv.module.scss";
 const CV = () => {
   return (
     <main className={styles.documentContainer}>
-      <iframe src="/cv.pdf" height="100%" />
+      <iframe
+        src="https://d26gnvwaf4fni9.cloudfront.net/documents/borys-melnyk-full-stack-cv-web.pdf"
+        height="100%"
+      />
     </main>
   );
 };
