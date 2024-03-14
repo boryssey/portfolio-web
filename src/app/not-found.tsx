a44168971c3e74cbed638b173a6e7a210b764963
Borys Melnyk <bor920@gmail.com>
2024-03-14 19:10:10 +0000

    feat: add Not Found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import styles from "./notFound.module.scss";
+
+const NotFoundPage = () => {
+  return (
+    <main className={styles.container}>
+      <p>404 Page Not Found</p>
+    </main>
+  );
+};
+
+export default NotFoundPage;
