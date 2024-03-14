fd44faee5bf025d47e68f5c5b3b5fcb0e98d289d
Borys Melnyk <bor920@gmail.com>
2024-03-14 19:08:14 +0000

    feat: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
 import type { Metadata } from "next";
-import { Manrope } from 'next/font/google'
-
+import { Manrope } from "next/font/google";
 import "./globals.scss";
 import NavBar from "@/components/NavBar";
+import styles from "./layout.module.scss";
+import Link from "next/link";
+import LinkedinIcon from "@/components/icons/LinkedinIcon";
+import GithubIcon from "@/components/icons/GithubIcon";
+import MediumIcon from "@/components/icons/MediumIcon";
 
 const manrope = Manrope({
-  variable: '--font-manrope',
-  subsets: ['latin', 'latin-ext'],
-}
-)
+  variable: "--font-manrope",
+  subsets: ["latin", "latin-ext"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +28,18 @@ export default function RootLayout({
       <body className={manrope.className}>
         <NavBar />
         {children}
-        </body>
+        <footer className={styles.footer}>
+          <Link href="https://linkedin.com/borys-melnyk">
+            <LinkedinIcon />
+          </Link>
+          <Link href="https://github.com/boryssey">
+            <GithubIcon />
+          </Link>
+          <Link href="https://boryssey.medium.com/">
+            <MediumIcon />
+          </Link>
+        </footer>
+      </body>
     </html>
   );
 }
