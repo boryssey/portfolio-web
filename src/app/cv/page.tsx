be4c0ccd4113fe471a78f5e9946287a30258c74b
Borys Melnyk <bor920@gmail.com>
2024-09-11 18:17:48 +0100

    feat: update cv link to r2

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -4,7 +4,7 @@ const CV = () => {
   return (
     <main className={styles.documentContainer}>
       <iframe
-        src="https://d26gnvwaf4fni9.cloudfront.net/documents/borys-melnyk-full-stack-cv-web.pdf"
+        src="https://r2-portfolio.boryssey.com/Borys-Melnyk-CV-redacted.pdf"
         height="100%"
       />
     </main>
