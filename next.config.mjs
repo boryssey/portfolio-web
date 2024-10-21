c7875cdb2ced3e1bcdcf7c937bddcd5c1d5e1e02
Borys Melnyk <bor920@gmail.com>
2024-10-21 10:49:13 +0100

    fix: image not loading in blog page

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.resolve.alias.canvas = false;
-        
-        return config;
-    },
-    images: {
-        remotePatterns: [
-            {   
-                protocol: 'https',
-                hostname: 'd26gnvwaf4fni9.cloudfront.net'
-            }
-        ]
-    }
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "r2-portfolio.boryssey.com",
+      },
+    ],
+  },
 };
 import withBundleAnalyzer from "@next/bundle-analyzer";
 const bundleAnalyzer = withBundleAnalyzer({
